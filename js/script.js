f2f62e75d0f1480837d1bfa6d38875e0aaf44a3a
AgoSuarez <agosuaort@gmail.com>
2020-06-19 20:46:43 +0100

    Improve some actions

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ function guardarElemento() {
 
 function LimpiarFormulario() {
   document.getElementById("tarea").value = "";
-  tareaInput.removeAttribute("disabled");
+  document.getElementById("tarea").removeAttribute("disabled");
   document.getElementById("index").value = "";
   document.getElementById("hecho").checked = false;
 }
